6db4cb31efa4e326ffd2ab54da7dcdeb6b9f84bb
Karl von Randow <karl@xk72.com>
2020-01-09 15:54:32 +1300

    Convert to libmaxminddb

--- README.rst
+++ README.rst
@@ -1,50 +1,33 @@
-==========
-vmod_geoip
-==========
-
-**important note: this vmod is now obsolete (as libgeoip itself is) and won't receive updates.**
+===========
+vmod_geoip2
+===========
 
 ---------------------------
-Varnish GeoIP Lookup Module
+Varnish GeoIP2 Lookup Module
 ---------------------------
 
-:Author: Hauke Lampe
-:Date: 2011-09-26
+:Author: Karl von Randow, Hauke Lampe
+:Date: 2020-01-09
 :Version: 1.0.3
 :Manual section: 3
 
 SYNOPSIS
 ========
 
-import geoip;
+import geoip2;
 
 DESCRIPTION
 ===========
 
-This Varnish module exports functions to look up GeoIP country codes.
-Requires GeoIP library (on Debian install libgeoip-dev)
+This Varnish module exports functions to look up GeoIP2 country codes.
+Requires MaxMind's libmaxminddb library (on Debian install libmaxminddb)
 
-Inspired by https://vrobert.fr/2010/07/another-way-to-link-varnish-and-maxmind-geoip/
+Forked from https://github.com/varnish/libvmod-geoip (which uses the legacy GeoIP library)
+and using code from https://github.com/nytm/varnish-mmdb-vmod
 
 
 FUNCTIONS
 =========
-
-ip_country_code (not exported yet)
-----------------------------------
-
-Prototype
-        ::
-
-                ip_country_code(IP I)
-Return value
-	STRING
-Description
-	Returns two-letter country code string from IP address
-Example
-        ::
-
-                set req.http.X-Country-Code = geoip.ip_country_code(client.ip);
 
 country_code
 ------------
@@ -62,23 +45,6 @@ Example
 
                 set req.http.X-Country-Code = geoip.country_code("127.0.0.1");
 
-
-ip_country_name (not exported yet)
-----------------------------------
-
-Prototype
-        ::
-
-                ip_country_name(IP I)
-Return value
-	STRING
-Description
-	Returns country name string from IP address
-Example
-        ::
-
-                set req.http.X-Country-Name = geoip.ip_country_name(client.ip);
-
 country_name
 ------------
 
@@ -95,24 +61,7 @@ Example
 
                 set req.http.X-Country-Name = geoip.country_name("127.0.0.1");
 
-
-ip_region_name (not exported yet)
----------------------------------
-
-Prototype
-        ::
-
-                ip_region_name(IP I)
-Return value
-	STRING
-Description
-	Returns region name string from IP address
-Example
-        ::
-
-                set req.http.X-Region-Name = geoip.ip_region_name(client.ip);
-
-region_name (not exported yet)
+region_name
 ------------------------------
 
 Prototype
@@ -128,6 +77,22 @@ Example
 
                 set req.http.X-Region-Name = geoip.region_name("127.0.0.1");
 
+city_name
+------------------------------
+
+Prototype
+        ::
+
+                city_name(STRING S)
+Return value
+        STRING
+Description
+        Returns city name string
+Example
+        ::
+
+                set req.http.X-City-Name = geoip.city_name("127.0.0.1");
+
 
 INSTALLATION
 ============
@@ -140,14 +105,14 @@ It is a prerequisite that you have the varnish-dev package installed as well as 
 
  apt-get install varnish-dev
 
-Install the GeoIP library headers::
+Install the MaxMindDB library headers::
 
- apt-get install libgeoip-dev
+ apt-get install libmaxminddb-dev
 
 To check out the current development source::
 
- git clone git://github.com/varnish/libvmod-geoip.git
- cd libvmod-geoip; ./autogen.sh
+ git clone git://github.com/karlvr/libvmod-geoip2.git
+ cd libvmod-geoip2; ./autogen.sh
 
 Usage::
 
@@ -159,12 +124,5 @@ Make targets:
 * sudo make install - installs your vmod
 * make check - runs the unit tests in ``src/tests/*.vtc``
 
-KNOWN ISSUES
-============
+Database:
 
-Certain versions of the GeoIP library will fail if the GeoIP database
-file is updated by overwriting the old file directly. 
-
-This could impact varnish stability, and it is therefore recommended
-to download the update as a new file, and then ``mv`` the new file
-onto the old active database.
---
+++ src/vmod_geoip2.c
@@ -0,0 +1,199 @@
+/**
+ * libvmod-geoip2 - varnish interface to MaxMind's libmaxminddb library
+ * libmaxminddb API: https://github.com/maxmind/libmaxminddb/releases
+ *
+ * See file README.rst for usage instructions
+ *
+ * This code is licensed under a MIT-style License, see file LICENSE
+*/
+
+#include <stdlib.h>
+#include <maxminddb.h>
+#include <string.h>
+
+#include "cache/cache.h"
+#include "vcl.h"
+
+#ifndef VRT_H_INCLUDED
+#include "vrt.h"
+#endif
+
+#include "vcc_if.h"
+
+#define DEBUG 0
+
+static MMDB_s mmdb_handle;
+
+// close gets called by varnish when then the threads destroyed
+void close_mmdb(void *mmdb_handle)
+{
+	// don't do anything if the db didn't open correctly.
+	if (mmdb_handle == NULL) {
+		return;
+	}
+	MMDB_s *handle = (MMDB_s *)mmdb_handle;
+	MMDB_close(handle);
+}
+
+VCL_INT
+vmod_init(VRT_CTX, struct vmod_priv *pp, const char *mmdb_path)
+{
+	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+
+    int mmdb_baddb = MMDB_open(mmdb_path, MMDB_MODE_MMAP, &mmdb_handle);
+    if (mmdb_baddb == MMDB_SUCCESS) {
+    	pp->priv = (void *)&mmdb_handle;
+    	pp->free = close_mmdb;
+    	return 0;
+    } else {
+        fprintf(stderr, "[ERROR] MMDB_open: Can't open %s - %s\n",
+                mmdb_path, MMDB_strerror(mmdb_baddb));
+        if (MMDB_IO_ERROR == mmdb_baddb) {
+            fprintf(stderr,
+                    "[ERROR] MMDB_open: IO error: %s\n",
+                    strerror(mmdb_baddb));
+        }
+        return 1;
+    }
+}
+
+/**
+ * geo_lookup takes a handle to the mmdb, an IP address and a lookup path
+ * and returns a string of the value or NULL.
+ * lookup_path is described in this doc: http://maxmind.github.io/MaxMind-DB/
+ */
+char *
+geo_lookup(MMDB_s *const mmdb_handle, const char *ipstr, const char **lookup_path)
+{
+	char *data = NULL;
+	// Lookup IP in the DB
+	int gai_error = 0;
+	int mmdb_error = 0;
+	MMDB_lookup_result_s result =
+		MMDB_lookup_string(mmdb_handle, ipstr, &gai_error, &mmdb_error);
+
+	if (0 != gai_error) {
+#ifdef DEBUG
+		fprintf(stderr,
+				"[INFO] Error from MMDB_lookup_string for %s - %s\n\n",
+				ipstr, gai_strerror(gai_error));
+#endif
+		return NULL;
+	}
+
+	if (MMDB_SUCCESS != mmdb_error) {
+#ifdef DEBUG
+		fprintf(stderr,
+				"[ERROR] Got an error from libmaxminddb: %s\n\n",
+				MMDB_strerror(mmdb_error));
+#endif
+		return NULL;
+	}
+
+	// Parse results
+	MMDB_entry_data_s entry_data;
+
+	if (result.found_entry) {
+		int status = MMDB_aget_value(&result.entry, &entry_data, lookup_path);
+
+		if (MMDB_SUCCESS != status) {
+#ifdef DEBUG
+			fprintf(
+				stderr,
+				"[WARN] Got an error looking up the entry data. Make sure \
+ the lookup_path is correct. %s\n",
+				MMDB_strerror(status));
+#endif
+			return NULL;
+		}
+
+		if (entry_data.has_data) {
+			switch(entry_data.type) {
+			case MMDB_DATA_TYPE_UTF8_STRING: {
+				data = strndup(entry_data.utf8_string, entry_data.data_size);
+				break;
+			}
+			case MMDB_DATA_TYPE_UINT16: {
+				uint16_t num = UINT16_MAX;
+				int len      = (int)((ceil(log10(num)))*sizeof(char));
+				data         = calloc(sizeof(char), len+1);
+				if (data != NULL) {
+					snprintf(data, len+1, "%u", entry_data.uint16);
+				}
+				break;
+			}
+			default:
+#ifdef DEBUG
+				fprintf(
+					stderr,
+					"[WARN] No handler for entry data type (%d) was found\n",
+					entry_data.type);
+#endif
+				break;
+			}
+		} else {
+			return NULL;
+		}
+	} else {
+#ifdef DEBUG
+		fprintf(
+			stderr,
+			"[INFO] No entry for this IP address (%s) was found\n",
+			ipstr);
+#endif
+		return NULL;
+	}
+	return data;
+}
+
+// Lookup a field
+VCL_STRING
+vmod_lookup(VRT_CTX, struct vmod_priv *global, const char *ipstr, const char **lookup_path)
+{
+    char *data = NULL;
+    char *cp   = NULL;
+    MMDB_s * mmdb_handle = (struct MMDB_s *)global->priv;
+
+    if (mmdb_handle == NULL) {
+        fprintf(stderr, "[WARN] varnish gave NULL maxmind db handle");
+        return cp;
+    }
+    data = geo_lookup(mmdb_handle, ipstr, lookup_path);
+
+    if (data != NULL) {
+        cp = WS_Copy(ctx->ws, data, strlen(data)+1);
+        free(data);
+    } else {
+        cp = WS_Copy(ctx->ws, "", 1);
+    }
+
+    return cp;
+}
+
+VCL_STRING
+vmod_country_code(VRT_CTX, struct vmod_priv *global, const char *ipstr)
+{
+    const char *lookup_path[] = {"country", "iso_code", NULL};
+    return vmod_lookup(ctx, global, ipstr, lookup_path);
+}
+
+VCL_STRING
+vmod_country_name(VRT_CTX, struct vmod_priv *global, const char *ipstr)
+{
+    const char *lookup_path[] = {"country", "names", "en", NULL};
+    return vmod_lookup(ctx, global, ipstr, lookup_path);
+}
+
+VCL_STRING
+vmod_region_name(VRT_CTX, struct vmod_priv *global, const char *ipstr)
+{
+    const char *lookup_path[] = {"subdivisions", "0", "names", "en", NULL};
+    return vmod_lookup(ctx, global, ipstr, lookup_path);
+}
+
+VCL_STRING
+vmod_city_name(VRT_CTX, struct vmod_priv *global, const char *ipstr)
+{
+    const char *lookup_path[] = {"city", "names", "en", NULL};
+    return vmod_lookup(ctx, global, ipstr, lookup_path);
+}
